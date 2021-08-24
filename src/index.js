182c61cd879887a151b17433a294f456e4b60a01
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-08-24 15:06:24 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+//can this be modulized? try pulling it out later
+const learnBasicJS = [
+  {
+    id: "001",
+    name: "Comment your code",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comment-your-javascript-code"
+  },
+  {
+    id: "002",
+    name: "declare variables",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/declare-javascript-variables"
+  },
+  {
+    id: "003",
+    name: "storing values with assignment operator",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/storing-values-with-the-assignment-operator"
+  },
+  {
+    id: "004",
+    name: "assigning value of one variable to another",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/assigning-the-value-of-one-variable-to-another"
+  }
+];
