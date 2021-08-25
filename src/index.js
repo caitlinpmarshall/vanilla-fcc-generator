e3bf3a03201e734aaeb359c61bd2f0df2c19195a
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-08-25 15:10:06 -0400

    Expands number of lessons in array, adds some basic styling

--- src/index.js
+++ src/index.js
@@ -1,48 +1,156 @@
 //ELEMENT SELECTORS
-const lesson = document.querySelector(".lesson");
+const lessonLink = document.querySelector(".lesson-link");
 const newLesson = document.querySelector(".new-lesson");
-//console.log(newLesson);
+//console.log(lessonLink.href);
 
 //LESSONS ARRAY
 //TODO: can this be modulized? try pulling it out later
+//TODO: add more lessons
+//TODO: add categories of lessons
 const learnBasicJS = [
   {
-    id: "001",
-    name: "Comment your code",
+    id: "0001",
+    name: "Comment Your JavaScript Code",
     link:
       "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comment-your-javascript-code"
   },
   {
-    id: "002",
-    name: "declare variables",
+    id: "0002",
+    name: "Declare JavaScript Variables",
     link:
       "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/declare-javascript-variables"
   },
   {
-    id: "003",
-    name: "storing values with assignment operator",
+    id: "0003",
+    name: "Storing Values with the Assignment Operator",
     link:
       "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/storing-values-with-the-assignment-operator"
   },
   {
-    id: "004",
-    name: "assigning value of one variable to another",
+    id: "0004",
+    name: "Assigning the Value of One Variable to Another",
     link:
       "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/assigning-the-value-of-one-variable-to-another"
+  },
+  {
+    id: "0005",
+    name: "Initializing Variables with the Assignment Operator",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/initializing-variables-with-the-assignment-operator"
+  },
+  {
+    id: "0006",
+    name: "Understanding Uninitialized Variables",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/understanding-uninitialized-variables"
+  },
+  {
+    id: "0007",
+    name: "Understanding Case Sensitivity in Variables",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/understanding-case-sensitivity-in-variables"
+  },
+  {
+    id: "0008",
+    name: "Add Two Numbers with JavaScript",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/add-two-numbers-with-javascript"
+  },
+  {
+    id: "0009",
+    name: "Subtract One Number from Another with JavaScript",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/subtract-one-number-from-another-with-javascript"
+  },
+  {
+    id: "0010",
+    name: "Multiply Two Numbers with JavaScript",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/multiply-two-numbers-with-javascript"
+  },
+  {
+    id: "0011",
+    name: "Divide One Number by Another with JavaScript",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/divide-one-number-by-another-with-javascript"
+  },
+  {
+    id: "0012",
+    name: "Increment a Number with JavaScript",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/increment-a-number-with-javascript"
+  },
+  {
+    id: "0013",
+    name: "Decrement a Number with JavaScript",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/decrement-a-number-with-javascript"
+  },
+  {
+    id: "0014",
+    name: "Create Decimal Numbers with JavaScript",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/create-decimal-numbers-with-javascript"
+  },
+  {
+    id: "0015",
+    name: "Multiply Two Decimals with JavaScript",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/multiply-two-decimals-with-javascript"
+  },
+  {
+    id: "0016",
+    name: "Divide One Decimal by Another with JavaScript",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/divide-one-decimal-by-another-with-javascript"
+  },
+  {
+    id: "0017",
+    name: "Finding a Remainder in JavaScript",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/finding-a-remainder-in-javascript"
+  },
+  {
+    id: "0018",
+    name: "Compound Assignment With Augmented Addition",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-addition"
+  },
+  {
+    id: "0019",
+    name: "Compound Assignment With Augmented Subtraction",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-subtraction"
+  },
+  {
+    id: "0020",
+    name: "Compound Assignment With Augmented Multiplication",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-multiplication"
+  },
+  {
+    id: "0021",
+    name: "Compound Assignment With Augmented Division",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/compound-assignment-with-augmented-division"
+  },
+  {
+    id: "0022",
+    name: "Declare String Variables",
+    link:
+      "https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/declare-string-variables"
   }
 ];
 
 //ON CLICK, NEW LESSON LINK GENERATED
 
-//event listener
 newLesson.addEventListener("click", function () {
   const randomIndex = Math.floor(Math.random() * learnBasicJS.length);
-
   //console.log(randomIndex);
-
-  const randomLesson = learnBasicJS[randomIndex].link;
-  lesson.innerHTML = randomLesson;
+  const randomLessonLink = learnBasicJS[randomIndex].link;
+  const randomLessonName = learnBasicJS[randomIndex].name;
+  lessonLink.href = randomLessonLink;
+  lessonLink.innerHTML = randomLessonName;
+  newLesson.innerHTML = "gimme another!";
   //console.log(randomLesson);
 });
-
-//TODO: link should go in hyperlink, text show name
