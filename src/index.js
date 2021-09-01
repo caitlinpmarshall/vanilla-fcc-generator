953283b60831bcc2f9684f9c74b78d85abb9099a
caitlinpmarshall <caitlinpmarshall@gmail.com>
2021-08-31 21:22:47 -0400

    javascript function now pulls from both basic js and regex arrays.  But, it's only tied to the first button.  Fix that.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { learnBasicJS } from "./data.js";
 import { regex } from "./data.js";
 
+//LEARN BASIC JS
 //ELEMENT SELECTORS
 const basicLessonLink = document.querySelector(".basicJS .lesson-link");
 const basicNewLesson = document.querySelector(".new-lesson");
@@ -16,5 +17,24 @@ basicNewLesson.addEventListener("click", function () {
   basicLessonLink.href = randomBasicLessonLink;
   basicLessonLink.innerHTML = randomBasicLessonName;
   basicNewLesson.innerHTML = "gimme another!";
+  //console.log(randomLesson);
+});
+
+//REGEX
+//ELEMENT SELECTORS
+const regexLessonLink = document.querySelector(".regex .lesson-link");
+const regexNewLesson = document.querySelector(".new-lesson");
+//console.log(regexLessonLink.href);
+
+//ON CLICK, NEW LESSON LINK GENERATED
+
+regexNewLesson.addEventListener("click", function () {
+  const randomIndex = Math.floor(Math.random() * regex.length);
+  //console.log(learnRegexJS[111].link);
+  const randomRegexLessonLink = regex[randomIndex].link;
+  const randomRegexLessonName = regex[randomIndex].name;
+  regexLessonLink.href = randomRegexLessonLink;
+  regexLessonLink.innerHTML = randomRegexLessonName;
+  regexNewLesson.innerHTML = "gimme another!";
   //console.log(randomLesson);
 });
